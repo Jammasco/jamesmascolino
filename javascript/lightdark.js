6d2c9ce81dc86f71875e1b2cd908114579d4c1e5
Jammasco <102613080+Jammasco@users.noreply.github.com>
2023-05-18 22:05:53 -0500

    Add files via upload

---
+++ javascript/lightdark.js
@@ -0,0 +1,30 @@
+const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg";
+let theOpacity = 2;
+
+function darklight2() {
+  document.getElementById("lightresume").classList = "fadeOut";
+  if (document.getElementById("darklight").src == "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg"){
+    document.getElementById("darklight").src = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg";
+    document.getElementById("lightresume").classList = "fadeIn";
+    } else{
+    document.getElementById("darklight").src = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+    document.getElementById("lightresume").classList = "fadeOut";
+    }
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+  var darkButton = document.querySelector("#darklight");
+  darkButton.classList.toggle("light-mode");
+}
+
+function darklight() {
+  if (document.getElementById("darklight").src == "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg"){
+    document.getElementById("darklight").src = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg";
+    } else{
+    document.getElementById("darklight").src = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
+    }
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+  var darkButton = document.querySelector("#darklight");
+  darkButton.classList.toggle("light-mode");
+}
