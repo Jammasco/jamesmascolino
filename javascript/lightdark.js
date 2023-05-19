f3dc87a2b07f8030ee43b907ce1c86522b4eb620
Jammasco <102613080+Jammasco@users.noreply.github.com>
2023-05-19 12:48:33 -0500

    Update lightdark.js

--- javascript/lightdark.js
+++ javascript/lightdark.js
@@ -1,15 +1,17 @@
 const sun = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
 const moon = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg";
-let theOpacity = 2;
 
 function darklight2() {
   document.getElementById("lightresume").classList = "fadeOut";
+  document.getElementById("darkresume").classList = "fadeIn";
   if (document.getElementById("darklight").src == "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg"){
     document.getElementById("darklight").src = "https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg";
     document.getElementById("lightresume").classList = "fadeIn";
+    document.getElementById("darkresume").classList = "fadeOut";
     } else{
     document.getElementById("darklight").src = "https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg";
     document.getElementById("lightresume").classList = "fadeOut";
+    document.getElementById("darkresume").classList = "fadeIn";
     }
   var body = document.body;
   body.classList.toggle("dark-mode");
